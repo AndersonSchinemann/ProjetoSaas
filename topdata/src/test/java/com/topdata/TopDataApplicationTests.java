dde1b19831c391f68e75e41eb070dd44f6ebd014
AndersonSchinemann <anderschinemann@gmail.com>
2023-11-16 09:31:26 -0300

    Teste para TopData_01

---
+++ topdata/src/test/java/com/topdata/TopDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
